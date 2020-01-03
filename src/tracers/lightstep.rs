1bd6f1551f877f1d15d12ac1e69007a677afe3ba
Daniel Neuhäuser <dasdasich@gmail.com>
2020-01-03 18:43:55 +0100

    store spans

--- src/tracers/lightstep.rs
+++ src/tracers/lightstep.rs
@@ -1,4 +1,5 @@
 use crate::api::{FinishedSpan, Reporter, SpanBuilder, SpanContext, SpanContextState, Tracer};
+use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
 
 /// Generated code based on lightstep-tracer-common/collector.proto
@@ -30,10 +31,46 @@ impl LightStepSpanContextState {
 impl SpanContextState for LightStepSpanContextState {}
 
 #[derive(Debug)]
-struct LightStepReporter {}
+struct LightStepReporter {
+    finished_spans_incoming: crossbeam_channel::Sender<FinishedSpan>,
+    finished_spans_outgoing: crossbeam_channel::Receiver<FinishedSpan>,
+    dropped_spans: AtomicUsize,
+}
+
+impl LightStepReporter {
+    fn new(buffer_capacity: usize) -> Self {
+        let (s, r) = crossbeam_channel::bounded(buffer_capacity);
+        LightStepReporter {
+            finished_spans_incoming: s,
+            finished_spans_outgoing: r,
+            dropped_spans: AtomicUsize::new(0),
+        }
+    }
+}
 
 impl Reporter for LightStepReporter {
-    fn report(&self, _finished_span: FinishedSpan) {}
+    fn report(&self, mut finished_span: FinishedSpan) {
+        // Add the finished_span to the channel, if that fails because the channel is full drop the
+        // oldest span from the channel and try again.
+        loop {
+            finished_span = match self.finished_spans_incoming.try_send(finished_span) {
+                Ok(()) => return,
+                Err(crossbeam_channel::TrySendError::Disconnected(_)) => {
+                    // Reporting thread is dead. We are going to have to drop this span.
+                    self.dropped_spans.fetch_add(1, Ordering::SeqCst);
+                    return;
+                }
+                Err(crossbeam_channel::TrySendError::Full(finished_span)) => finished_span,
+            };
+
+            // We don't care about the result of this call because:
+            // 1. If it's Ok, we're good.
+            // 2. If it's Empty, we're also good.
+            // 3. It can't be disconnected because we hold a sender.
+            let _ = self.finished_spans_outgoing.try_recv();
+            self.dropped_spans.fetch_add(1, Ordering::SeqCst);
+        }
+    }
 }
 
 #[derive(Clone, Debug)]
@@ -43,8 +80,9 @@ pub struct LightStepTracer {
 
 impl LightStepTracer {
     pub fn new() -> Self {
+        // TODO: configuration
         LightStepTracer {
-            reporter: Arc::new(LightStepReporter {}),
+            reporter: Arc::new(LightStepReporter::new(64)),
         }
     }
 }
