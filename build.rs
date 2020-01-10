0e35badf59810977a8437102621d00eae0567878
Daniel Neuhäuser <dasdasich@gmail.com>
2020-01-10 14:22:10 +0100

    stop changing files outside of OUT_DIR in build.rs

--- build.rs
+++ build.rs
@@ -16,18 +16,6 @@ mod implementation {
         // directory changes.
         println!("cargo:rerun-if-changed=lightstep-tracer-common");
 
-        Command::new("git")
-            .args(&[
-                "clone",
-                // Clone at tag v1.0.3
-                "--branch=v1.0.3",
-                // Clone only one commit to speed up the process
-                "--depth=1",
-                "https://github.com/lightstep/lightstep-tracer-common.git",
-            ])
-            .output()
-            .expect("Cloning lightstep/lightstep-tracer-common repository failed");
-
         prost_build::compile_protos(
             &[
                 "lightstep-tracer-common/collector.proto",
